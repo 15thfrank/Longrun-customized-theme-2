28d502bc5d92ceef94bb36bf860a4a21ac525b4d
Seehydee <92515518+15thfrank@users.noreply.github.com>
2023-02-25 04:01:11 +0100

    Final adjustments???

--- assets/custom.js
+++ assets/custom.js
@@ -3,3 +3,15 @@
 /-----------------------------------------------------------------------------*/
 
 // Insert any custom theme js here...
+const searchBtn = document.getElementById('ch_SearchBtn');
+const searchModal = document.getElementById('cs_searchModal');
+const searchWrapper = document.getElementById('cs_searchWrapper');
+const modalX = document.getElementById('ch_searchModalX');
+
+searchBtn.addEventListener('click', function(){
+    searchModal.style.display = "flex";
+})
+
+modalX.addEventListener('click', function(){
+    searchModal.style.display = "none";
+})
