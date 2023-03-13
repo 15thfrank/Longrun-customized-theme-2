ed32ded1f1baba27c342cb3236911db52623f629
Seehydee <92515518+15thfrank@users.noreply.github.com>
2023-03-13 17:40:47 +0100

    Final mod. A little extra bit for UX.

--- assets/custom.js
+++ assets/custom.js
@@ -15,3 +15,12 @@ searchBtn.addEventListener('click', function(){
 modalX.addEventListener('click', function(){
     searchModal.style.display = "none";
 })
+
+
+const mobileNavswitch = () => {
+    if (document.getElementById("mobile_menuIcon").src == "https://cdn.shopify.com/s/files/1/0271/4045/6534/files/menu.png?v=1676473415") {
+        document.getElementById("mobile_menuIcon").src = "https://img.icons8.com/material-outlined/24/ffffff/delete-sign.png";
+    } else {
+        document.getElementById("mobile_menuIcon").src = "https://cdn.shopify.com/s/files/1/0271/4045/6534/files/menu.png?v=1676473415";
+    }
+}
